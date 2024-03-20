0f39a844fd1da07b98bd86d8e6cfd6e822cde289
Thai Quang Minh <minhtq@notavn.com>
2024-03-20 09:35:23 +0700

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import path from "path";
 import dotenv from "dotenv"
 import cookieParser from 'cookie-parser'
 import authRoutes from './routes/auth.routes.js'
@@ -8,6 +9,9 @@ import connectToMongoDb from './db/connectToMongoDb.js'
 import { app, server } from './socket/socket.js'
 
 const port = process.env.PORT || 5000
+
+const __dirname = path.resolve()
+console.log(__dirname)
 
 // Enviroments
 dotenv.config()
@@ -19,6 +23,13 @@ app.use(cookieParser())
 app.use('/api/auth', authRoutes)
 app.use('/api/message', messageRoutes)
 app.use('/api/user', userRoutes)
+
+// Deployment
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(port, () => {
   connectToMongoDb()
