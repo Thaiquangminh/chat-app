ddaf1ef988d94a715a056a72c175cc18c865b9f2
Thai Quang Minh <minhtq@notavn.com>
2024-03-20 17:53:04 +0700

    Add notification img

--- frontend/src/hooks/useListenMessages.js
+++ frontend/src/hooks/useListenMessages.js
@@ -15,12 +15,14 @@ const useListenMessages = () => {
     if (Notification.permission === "granted") {
       new Notification("New Message Received", {
         body: "Bạn nhận được 1 thông báo mới!",
+        icon: '/messenger.png'
       });
     } else if (Notification.permission !== "denied") {
       Notification.requestPermission().then((permission) => {
         if (permission === "granted") {
           new Notification("New Message Received", {
             body: "Bạn nhận được 1 thông báo mới!",
+            icon: '/messenger.png'
           });
         }
       });
